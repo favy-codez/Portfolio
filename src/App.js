2843b445d33d553dd6992c1c4bc1bba829f3ea07
favycodezz <ezeliorafavour81@gmail.com>
2024-05-14 05:52:33 -0700

    added files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
-import './App.css';
+import './App.scss';
 // import "./index.css";
 
 function App() {
